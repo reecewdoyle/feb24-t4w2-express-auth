36d87aae698adc38559d338b5293f4aeb6159ebc
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-11-08 16:58:11 +1100

    feat: jwt functions scaffolded and user model created

---
+++ src/jwtFunctions.js
@@ -0,0 +1,21 @@
+
+let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+// async function generateJWT(userDetailsObj)
+async function generateJWT(userId, username, roles = null){
+    
+}
+
+async function decodeJWT(tokenToDecode){
+    
+}
+
+async function validateUserAuth(request, response, next){
+    
+}
+
+module.exports = {
+    generateJWT,
+    decodeJWT,
+    validateUserAuth
+}
